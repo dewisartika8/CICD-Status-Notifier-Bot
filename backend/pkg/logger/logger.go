1fed1a9937e7a81d0b7ceaf411ff38942a01c037
arif.dwi <arif.dwi@peruridigit.co.id>
2025-07-29 14:11:08 +0700

    fix: log file

--- backend/pkg/logger/logger.go
+++ backend/pkg/logger/logger.go
@@ -25,13 +25,13 @@ func NewLogger() *logrus.Logger {
 	// On Windows, don't create symlinks to avoid permission issues
 	if runtime.GOOS == "windows" {
 		writer, err = rotatelogs.New(
-			path+".%Y%m%d",
+			path+".%Y%m%d.log",
 			rotatelogs.WithRotationTime(24*time.Hour),
 			rotatelogs.WithMaxAge(7*24*time.Hour),
 		)
 	} else {
 		writer, err = rotatelogs.New(
-			path+".%Y%m%d",
+			path+".%Y%m%d.log",
 			rotatelogs.WithLinkName(path),
 			rotatelogs.WithRotationTime(24*time.Hour),
 			rotatelogs.WithMaxAge(7*24*time.Hour),
